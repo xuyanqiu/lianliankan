b46728b4945f9c16a557b966a2450e335bafd9df
xuyanqiu <42957289+xuyanqiu@users.noreply.github.com>
2018-11-09 14:31:21 +0800

    连连看
    连连看

---
+++ 复制 script.js
@@ -0,0 +1,12 @@
+﻿
+/*
+ * 初始化函数
+ */
+function init() {
+	
+	//开始按钮
+	var startBtn = document.getElementById('startBtn');
+	startBtn.style.disable = true;
+	startBtn.value = '游戏进行中';
+	startBtn.style.background = '#666';
+}
